8213f2e1e5f2deeb12fabea6b9f01b29910dfcfb
Debarka Chakraborti <debarka_chakraborti@epam.com>
2025-03-09 02:12:43 +0530

    added files for second task

---
+++ task02/app/lambdas/hello_world/index.js
@@ -0,0 +1,22 @@
+const routes = {
+    "/hello": {
+        GET: () => ({
+            statusCode: 200,
+            message: "Hello from Lambda",
+        }),
+    },
+};
+
+exports.handler = async (event) => {
+    const path = event.rawPath || "/";
+    const method = event.requestContext?.http?.method || "UNKNOWN";
+
+    const routeHandler = routes[path]?.[method];
+
+    return routeHandler
+        ? routeHandler()
+        : {
+            statusCode: 400,
+            message:`Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+        };
+};
