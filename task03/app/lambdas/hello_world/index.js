4e46a4ebc770b4c39b34da440dc874c40a54144e
Debarka Chakraborti <debarka_chakraborti@epam.com>
2025-03-11 16:10:29 +0530

    updated files

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,9 +1,19 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        "statusCode": 200,
-        "message": "Hello from Lambda",
-        headers: { "Content-Type": "application/json" },
-    };
+    let response;
+
+    if (event.resource === "/hello" && event.httpMethod === "GET") {
+        response = {
+            "statusCode": 200,
+            "body": JSON.stringify({
+                "statusCode": 200,
+                "message": "Hello from Lambda"
+            }),
+            "headers": {
+                "content-type": "application/json"
+            },
+            "isBase64Encoded": false
+        };
+    }
+
     return response;
 };
