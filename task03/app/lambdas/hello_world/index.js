dfb3fef06fbbd9e2704e4079a739998c4d1b9cda
Debarka Chakraborti <debarka_chakraborti@epam.com>
2025-03-11 15:31:59 +0530

    updated files

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,36 +1,8 @@
-const routes = {
-    "/hello": {
-        GET: () => ({
-            "statusCode": 200,
-            "body": JSON.stringify({
-                "statusCode": 200,
-                "message": "Hello from Lambda"
-            }),
-            "headers": {
-              "content-type": "application/json"
-            },
-            "isBase64Encoded": false
-          }),
-    },
-};
-
 exports.handler = async (event) => {
-    const path = event.rawPath || "/";
-    const method = event.requestContext?.http?.method || "UNKNOWN";
-
-    const routeHandler = routes[path]?.[method];
-
-    return routeHandler
-        ? routeHandler()
-        : {
-            "statusCode": 400,
-            "body": JSON.stringify({
-                statusCode: 400,
-                message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
-              }),
-            "headers": {
-              "content-type": "application/json"
-            },
-            "isBase64Encoded": false
-          };
+    // TODO implement
+    const response = {
+        "statusCode": 200,
+        "message": "Hello from Lambda",
+    };
+    return response;
 };
